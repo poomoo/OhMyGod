d27a9bdd3b8c2184d7f45462188c9c6412a65452
poomoo <454592359@qq.com>
2015-12-30 17:10:33 +0800

    at co 12.30  third

--- app/src/main/java/com/poomoo/ohmygod/view/activity/SettingActivity.java
+++ app/src/main/java/com/poomoo/ohmygod/view/activity/SettingActivity.java
@@ -12,6 +12,21 @@ import android.view.View;
 import com.poomoo.ohmygod.R;
 import com.poomoo.ohmygod.utils.MyUtil;
 import com.poomoo.ohmygod.utils.SPUtils;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.media.QQShareContent;
+import com.umeng.socialize.media.QZoneShareContent;
+import com.umeng.socialize.media.SinaShareContent;
+import com.umeng.socialize.media.SmsShareContent;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.sso.QZoneSsoHandler;
+import com.umeng.socialize.sso.SinaSsoHandler;
+import com.umeng.socialize.sso.SmsHandler;
+import com.umeng.socialize.sso.UMQQSsoHandler;
+import com.umeng.socialize.weixin.controller.UMWXHandler;
+import com.umeng.socialize.weixin.media.CircleShareContent;
+import com.umeng.socialize.weixin.media.WeiXinShareContent;
 
 /**
  * 设置
@@ -20,6 +35,12 @@ import com.poomoo.ohmygod.utils.SPUtils;
  */
 public class SettingActivity extends BaseActivity {
     public static SettingActivity instance;
+
+    // 首先在您的Activity中添加如下成员变量
+    public static final UMSocialService mController = UMServiceFactory.getUMSocialService("com.umeng.share");
+    private String content = "天呐APP";
+    private String website = "http://zgqg.91jiaoyou.cn/zgqg";
+    private String title = "天呐";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +108,14 @@ public class SettingActivity extends BaseActivity {
      * @param view
      */
     public void toShareSoft(View view) {
-        MyUtil.showToast(getApplicationContext(), "分享软件");
+        // 配置需要分享的相关平台
+        configPlatforms();
+        // 设置分享内容
+        shareContent();
+        // 是否只有已登录用户才能打开分享选择页
+        mController.getConfig().setPlatforms(SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE, SHARE_MEDIA.WEIXIN,
+                SHARE_MEDIA.WEIXIN_CIRCLE, SHARE_MEDIA.SINA, SHARE_MEDIA.TENCENT, SHARE_MEDIA.SMS);
+        mController.openShare(this, false);
     }
 
     /**
@@ -137,5 +165,131 @@ public class SettingActivity extends BaseActivity {
             }
         }).create();
         dialog.show();
+    }
+
+    /**
+     * 配置分享平台参数</br>
+     */
+    private void configPlatforms() {
+        // 添加新浪SSO授权
+        mController.getConfig().setSsoHandler(new SinaSsoHandler());
+
+        // 添加QQ、QZone平台
+        addQQQZonePlatform();
+
+        // 添加微信、微信朋友圈平台
+        addWXPlatform();
+
+        // 添加短信平台
+        addSMS();
+    }
+
+    public void shareContent() {
+        // 本地图片
+        UMImage localImage = new UMImage(this, R.drawable.ic_logo);
+
+        // 配置SSO
+        mController.getConfig().setSsoHandler(new SinaSsoHandler());
+
+        // QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(this,
+        // "100424468", "c7394704798a158208a74ab60104f0ba");
+
+        WeiXinShareContent weixinContent = new WeiXinShareContent();
+        weixinContent.setShareContent(content);
+        weixinContent.setTitle(title);
+        weixinContent.setTargetUrl(website);
+        weixinContent.setShareMedia(localImage);
+        mController.setShareMedia(weixinContent);
+
+        // 设置朋友圈分享的内容
+        CircleShareContent circleMedia = new CircleShareContent();
+        circleMedia.setShareContent(content);
+        circleMedia.setTitle(title);
+        circleMedia.setShareMedia(localImage);
+        circleMedia.setTargetUrl(website);
+        mController.setShareMedia(circleMedia);
+
+        // 设置QQ空间分享内容
+        QZoneShareContent qzone = new QZoneShareContent();
+        qzone.setShareContent(content);
+        qzone.setTargetUrl(website);
+        qzone.setTitle(title);
+        qzone.setShareMedia(localImage);
+        mController.setShareMedia(qzone);
+
+        // 设置QQ分享内容
+        QQShareContent qqShareContent = new QQShareContent();
+        qqShareContent.setShareContent(content);
+        qqShareContent.setTitle(title);
+        qqShareContent.setShareMedia(localImage);
+        qqShareContent.setTargetUrl(website);
+        mController.setShareMedia(qqShareContent);
+
+        // 设置短信分享内容
+        SmsShareContent sms = new SmsShareContent();
+        sms.setShareContent(content);
+        sms.setShareImage(localImage);
+        mController.setShareMedia(sms);
+
+        // 设置新浪微博分享内容
+        SinaShareContent sinaContent = new SinaShareContent();
+        sinaContent.setShareContent(content);
+        sinaContent.setShareImage(localImage);
+        mController.setShareMedia(sinaContent);
+
+    }
+
+    /**
+     * @return
+     * @功能描述 : 添加QQ平台支持 QQ分享的内容， 包含四种类型， 即单纯的文字、图片、音乐、视频. 参数说明 : title, summary,
+     * image url中必须至少设置一个, targetUrl必须设置,网页地址必须以"http://"开头 . title :
+     * 要分享标题 summary : 要分享的文字概述 image url : 图片地址 [以上三个参数至少填写一个] targetUrl
+     * : 用户点击该分享时跳转到的目标地址 [必填] ( 若不填写则默认设置为友盟主页 )
+     */
+    private void addQQQZonePlatform() {
+        String appId = "100424468";
+        String appKey = "c7394704798a158208a74ab60104f0ba";
+        // 添加QQ支持, 并且设置QQ分享内容的target url
+        UMQQSsoHandler qqSsoHandler = new UMQQSsoHandler(this, appId, appKey);
+        qqSsoHandler.setTargetUrl(website);
+        qqSsoHandler.addToSocialSDK();
+
+        // 添加QZone平台
+        QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(this, appId, appKey);
+        qZoneSsoHandler.addToSocialSDK();
+    }
+
+    /**
+     * @return
+     * @功能描述 : 添加微信平台分享
+     */
+    private void addWXPlatform() {
+        // 注意：在微信授权的时候，必须传递appSecret
+        // wx967daebe835fbeac是你在微信开发平台注册应用的AppID, 这里需要替换成你注册的AppID
+        String appId = "wx55e834ca0a0327a6";
+        String appSecret = "5bb696d9ccd75a38c8a0bfe0675559b3";
+        // 添加微信平台
+        UMWXHandler wxHandler = new UMWXHandler(this, appId, appSecret);
+        wxHandler.addToSocialSDK();
+
+        // 支持微信朋友圈
+        UMWXHandler wxCircleHandler = new UMWXHandler(this, appId, appSecret);
+        wxCircleHandler.setToCircle(true);
+        wxCircleHandler.addToSocialSDK();
+    }
+
+    /**
+     * 添加短信平台
+     */
+    private void addSMS() {
+        // 添加短信
+        SmsHandler smsHandler = new SmsHandler();
+        smsHandler.addToSocialSDK();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mController.getConfig().cleanListeners();
     }
 }
