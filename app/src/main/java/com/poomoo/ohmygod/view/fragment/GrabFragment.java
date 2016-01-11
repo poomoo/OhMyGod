383fb6588d369a3e7e0c4f2e83631effef25c8d4
poomoo <454592359@qq.com>
2016-01-11 18:00:54 +0800

    at co 1.11

--- app/src/main/java/com/poomoo/ohmygod/view/fragment/GrabFragment.java
+++ app/src/main/java/com/poomoo/ohmygod/view/fragment/GrabFragment.java
@@ -474,10 +474,10 @@ public class GrabFragment extends BaseFragment implements OnItemClickListener, O
         if (!MyUtil.isLogin(getActivity()))
             return;
 
-//        if (!application.getLocateCity().equals(application.getCurrCity())) {
-//            MyUtil.showToast(getActivity().getApplicationContext(), application.getLocateCity() + "不能参加" + application.getCurrCity() + "的活动!");
-//            return;
-//        }
+        if (!application.getLocateCity().equals(application.getCurrCity())) {
+            MyUtil.showToast(getActivity().getApplicationContext(), application.getLocateCity() + "不能参加" + application.getCurrCity() + "的活动!");
+            return;
+        }
         if (grabBOList.get(position).getStatus() == 1) {
 //            LogUtils.i("lmf", "首页时间:" + adapter.getCountDownUtils().get(position).getMillisUntilFinished() + "");
             Bundle pBundle = new Bundle();
