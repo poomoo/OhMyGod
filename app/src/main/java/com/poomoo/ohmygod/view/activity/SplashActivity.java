edf672430bf0d5932e85b4d6ecfe92a5a7229f51
poomoo <454592359@qq.com>
2016-02-18 16:45:47 +0800

    at co 2.18 6.0系统下定位问题

--- app/src/main/java/com/poomoo/ohmygod/view/activity/SplashActivity.java
+++ app/src/main/java/com/poomoo/ohmygod/view/activity/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.poomoo.ohmygod.view.activity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
@@ -24,6 +28,7 @@ import org.litepal.tablemanager.Connector;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class SplashActivity extends BaseActivity {
     private static String DB_PATH = "/data/data/com.poomoo.ohmygod/databases/";
@@ -33,6 +38,8 @@ public class SplashActivity extends BaseActivity {
     public LocationClient mLocationClient = null;
     public BDLocationListener myListener = new MyLocationListener();
     private boolean isIndex = false;//是否需要引导
+    private int SDK_PERMISSION_REQUEST = 123;
+    private String permissionInfo="";
 
 
     @Override
@@ -42,6 +49,7 @@ public class SplashActivity extends BaseActivity {
 
 //        SQLiteDatabase db = Connector.getDatabase();
         importDB();        // 导入数据库文件
+        getPersimmions();
         mLocationClient = new LocationClient(getApplicationContext()); // 声明LocationClient类
         mLocationClient.registerLocationListener(myListener);
         initLocation();
@@ -146,6 +154,53 @@ public class SplashActivity extends BaseActivity {
             }
 
         } catch (Exception e) {
+        }
+    }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+                        /*
+                         * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+                         */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)) {
+                return true;
+            } else {
+                permissionsList.add(permission);
+                return false;
+            }
+
+        } else {
+            return true;
         }
     }
 }
